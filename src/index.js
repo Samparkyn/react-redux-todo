73fb4a50ec283dabe1904eec0cd931f02c74d741
Samparkyn <samparkyn101@gmail.com>
2018-11-19 13:20:08 +0100

    Add dependencies & update index.js to use Redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// root reducer
+import rootReducer from './reducers/index';
+// react redux
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// create redux store
+const store = createStore(rootReducer);
+
+// use a special React Redux component called <Provider> to magically make the store available 
+// to all container components in the application without passing it explicitly
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 serviceWorker.unregister();
